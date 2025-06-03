5e5597aa0458699fee4fbd80bbcbebaa91d5a519
Jota <33913578+yourch22@users.noreply.github.com>
2025-06-03 00:37:16 -0500

    Modificamos endpoind a rutas con buenas practicas en modulo SecurityConfig.java linea 54 /api/v1/**

--- src/main/java/com/api/muebleria/armadirique/config/SecurityConfig.java
+++ src/main/java/com/api/muebleria/armadirique/config/SecurityConfig.java
@@ -15,8 +15,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -51,7 +49,7 @@ public class SecurityConfig {
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/generate-token", "/api/v1/users").permitAll() /* se agrega correcto rutas con buenas practicas*/
+                        .requestMatchers("/api/v1/generate-token", "/api/v1/usuarios").permitAll() /* se agrega correcto rutas con buenas practicas*/
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
                 );
