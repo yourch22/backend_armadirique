068f4cabb464429bdaf859e2d320eae3e0ce73ae
Jota <33913578+yourch22@users.noreply.github.com>
2025-06-07 02:13:33 -0500

    Agregamos paginacion a productos

--- src/main/java/com/api/muebleria/armadirique/modules/producto/controller/ProductoController.java
+++ src/main/java/com/api/muebleria/armadirique/modules/producto/controller/ProductoController.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Page; // <--- ADD THIS LINE
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable; // Ensure this is present
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault; // Ensure this is present
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
