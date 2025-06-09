1e5b92bf612fbaea2c176f7d6296c970761941aa
DiegoSolorzano28 <diegoalessandrosolorzano@gmail.com>
2025-06-08 22:01:43 -0500

    Implementación de Apache POI #2

--- src/main/java/com/api/muebleria/armadirique/modules/productoReport/ListarProductoExcel.java
+++ src/main/java/com/api/muebleria/armadirique/modules/productoReport/ListarProductoExcel.java
@@ -1,10 +1,14 @@
 package com.api.muebleria.armadirique.modules.productoReport;
 
+import java.util.List;
 import java.util.Map;
 
+import com.api.muebleria.armadirique.modules.producto.entity.Producto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
@@ -18,5 +22,36 @@ public class ListarProductoExcel extends AbstractXlsxView {
                                       HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setHeader("Content-Disposition", "attachment; filename=\"listado-productos.xlsx\"");
         Sheet hoja = workbook.createSheet("Productos");
+
+        Row filaTitulo = hoja.createRow(0);
+        Cell celda = filaTitulo.createCell(0);
+        celda.setCellValue("LISTADO DE STOCK DE PRODUCTOS");
+
+        Row filaData = hoja.createRow(2);
+        String[] columnas = {"ID","NOMBRES", "DESCRIPCION", "PRECIO", "STOCK",
+                "IMAGEN_URL", "ESTADO", "ID CATEGORIA", "ID USUARIO", "NOMBRE USUARIO" };
+        for (int i = 0; i < columnas.length; i++) {
+            celda = filaData.createCell(i);
+            celda.setCellValue(columnas[i]);
+        }
+
+        List<Producto> listaC = (List<Producto>) model.get("productos");
+
+        int numFila=3;
+        for (Producto producto : listaC) {
+            filaData = hoja.createRow(numFila);
+
+            filaData.createCell(0).setCellValue(producto.getProductoId());
+            filaData.createCell(0).setCellValue(producto.getNombre());
+            filaData.createCell(0).setCellValue(producto.getDescripcion());
+            filaData.createCell(0).setCellValue(producto.getPrecio().toString());
+            filaData.createCell(0).setCellValue(producto.getStock());
+            filaData.createCell(0).setCellValue(producto.getImagenUrl());
+            filaData.createCell(0).setCellValue(producto.isEstado());
+            filaData.createCell(0).setCellValue(producto.getCategoria().getCategoriaId());
+            filaData.createCell(0).setCellValue(producto.getUsuario().getUsuarioId());
+
+            numFila ++;
+        }
     }
 }
